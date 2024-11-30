677982e86532bbd4429d5c309da928f73d5058ef
shrouk <shroukdawood1990@gmail.com>
2024-11-30 13:05:38 +0200

    commit04

--- assets/jscod/registration.js
+++ assets/jscod/registration.js
@@ -9,3 +9,29 @@ regformebtn.addEventListener("click" , function(e){
     }
     console.log(userData);
 });
+
+
+const regformInput = document.querySelectorAll("#registration input");
+regformInput.forEach((input) => {
+    
+    input.addEventListener("change" , function(){
+        // console.log(this.name , this.value);
+      const errorMassage= document.querySelector(`#error-${this.name}`);
+      errorMassage.innerHTML = "";
+
+      console.log(this.name, this.value);
+      const regEmail = /^[^\s@]+@[^\s@]+\.+[^\s@]+$/;
+
+      if (this.name ==="userEmail" && !regEmail.test(this.value))
+      {
+        console.log("Email not valid");
+        errorMassage.innerHTML ="Email not valid";
+      }
+
+     if (this.name ==="userPassword" && this.value.length < 8)
+        { 
+            console.log("password not valid");
+        errorMassage.innerHTML ="password not valid";
+        }   
+    });
+});
